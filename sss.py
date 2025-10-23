f6ae0f84a0d05fe7dce023accac9287a0f887b8c
Axel <axelmail22@gmail.com>
2025-10-23 19:46:55 +0200

    init

---
+++ sss.py
@@ -0,0 +1,15 @@
+# AEGIS - NowBlackout ENSIBS 2025
+# @uthor: Axel JEMAI
+# Last modified: 2025-20-10
+# Implementation of the SSS (Shamir's Secret Sharing) algorithm
+
+import random
+from sslib import shamir
+required_shares = 2
+distributed_shares = 5
+obj = shamir.to_base64(shamir.split_secret("this is my secret".encode('ascii'), required_shares, distributed_shares))
+print(obj)
+pm=obj[']
+data = {'required_shares': required_shares, 'prime_mod': obj['prime_mod'], 'shares': [obj['shares'][1], obj['shares'][4]]}
+msg = shamir.recover_secret(shamir.from_hex(data)).decode('ascii')
+print(msg)
